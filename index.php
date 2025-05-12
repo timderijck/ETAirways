803f9280e65087e3b39a3bcce181742e812078a6
timderijck <timderijck2@gmail.com>
2025-05-12 09:13:46 +0200

    eerste opzet project

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <header>
+
+    </header>
+
+    <main>
+        <h1>ET Airways</h1>
+
+        <?php phpinfo()?>
+    </main>
+    
+    <footer>
+
+    </footer>
+</body>
+</html>
