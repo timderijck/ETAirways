b57dea8ebefbb57601f2c532e47129d6ee12dea2
timderijck <timderijck2@gmail.com>
2025-05-12 11:31:31 +0200

    header design af, niet werkendof aangemaakte javascript, alleen html/css

--- index.php
+++ index.php
@@ -2,17 +2,49 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
 </head>
 <body>
-    <header>
-
+    <header class="flex">
+        <div class="maatschappijinfo flex">
+            <img src="assets/img/ETAirways.png" alt="ETplaatje" height="80" width="80">
+            <div class="maatschappijtekst">
+                <h1>ET Airways</h1>
+            </div>
+            <div class="taalinloggen flex">
+                <div class="taal flex">
+                    <img src="assets/img/nlvlag.png" alt="nl" width="27" height="20">
+                    <div class="nltekst">
+                        <h2>Netherlands - nl</h2>
+                    </div>
+                </div>
+                <div class="inloggen flex">
+                    <div class="inlogplaatje">
+                        <img src="assets/img/inlog.png" alt="inlog" width="32" height="32">
+                    </div>
+                    <div class="nltekst">
+                        <h2>Inloggen</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="hoofdteksten flex">
+            <div class="hoofdtekstpositie flex">
+                <h1>Home</h1>
+                <h1>Mijn Reis</h1>
+                <h1>Informatie</h1>
+                <h1>Hulp</h1>
+            </div>
+        </div>
     </header>
 
     <main>
-        <h1>ET Airways</h1>
+        
 
-        <?php phpinfo()?>
     </main>
     
     <footer>
