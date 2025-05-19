4583a293b7ad60344cb61173182feeed15832d2c
timderijck <timderijck2@gmail.com>
2025-05-19 10:32:50 +0200

    voorpagina van de main eerste deel af, nog geen werkende links

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="assets/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
 </head>
+
 <body>
     <header class="flex">
         <div class="maatschappijinfo flex">
@@ -15,7 +18,7 @@
             <div class="maatschappijtekst">
                 <h1>ET Airways</h1>
             </div>
-            <div class="taalinloggen flex">
+            <div class="taalinloggen flex position">
                 <div class="taal flex">
                     <img src="assets/img/nlvlag.png" alt="nl" width="27" height="20">
                     <div class="nltekst">
@@ -33,7 +36,7 @@
             </div>
         </div>
         <div class="hoofdteksten flex">
-            <div class="hoofdtekstpositie flex">
+            <div class="hoofdtekstpositie flex position">
                 <h1>Home</h1>
                 <h1>Mijn Reis</h1>
                 <h1>Informatie</h1>
@@ -41,14 +44,61 @@
             </div>
         </div>
     </header>
-
     <main>
-        
-
+        <div class="vluchtfoto flex">
+            <img src="assets/img/vluchtfoto.png" alt="vluchtfoto" width="1250" height="710">
+        </div>
+        <div class="vertrekblok flex position">
+            <div class="witteboxes flex position">
+                <div class="wittebox1 flex">
+                    <div class="omgedraaidimg">
+                        <img src="assets/img/vliegtuigomgedraaid.png" alt="" width="20" height="15">
+                    </div>
+                    <h2>Retourreis</h2>
+                    <div class="dropdownbox">
+                        <img src="assets/img/dropdown.png" alt=""  width="15" height="13">
+                    </div>
+                </div>
+                <div class="wittebox2 wittebox1 flex">
+                    <div class="omgedraaidimg extraruimte">
+                        <img src="assets/img/opstijgen.png" alt="" width="20" height="15">
+                    </div>
+                    <h2>Vertrek vanaf</h2>
+                </div>
+                <div class="wittebox3 wittebox2 wittebox1 flex">
+                    <div class="omgedraaidimg extraruimte">
+                        <img src="assets/img/vliegtuig.png" alt="" width="20" height="15">
+                    </div>
+                    <h2>Aankomst op</h2>
+                </div>
+            </div>
+        </div>
+        <div class="dealsbox position"></div>
+        <div class="dealstekst position">
+            <h3>Wilt u graag de beste vakantie voor uw gezin/familie boeken?</h3>
+            <br>
+            <h3>Bekijk dan nu onze vakantie <br>deals!</h3>
+        </div>
+        <div class="naardeals flex position">
+            <h4><a href="">Naar Deals →</a></h4>
+        </div>
+        <div class="blauweboxes flex position">
+            <div class="blauwebox flex">
+                <h2>Een vlucht boeken</h2>
+            </div>
+            <div class="blauwebox flex">
+                <h2>Boek met miles</h2>
+            </div>
+            <div class="blauwebox flex">
+                <h2>ET holidays</h2>
+            </div>
+        </div>
+        <div class="splitslijn position"></div>
     </main>
-    
+
     <footer>
 
     </footer>
 </body>
+
 </html>
