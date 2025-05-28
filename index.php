d24169291241bf7676ce5e3ec1eac23a723dbb59
timderijck <timderijck2@gmail.com>
2025-05-28 09:01:41 +0200

    voorpagina foto schaduw gegeven en begonnen met login.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com">
---
+++ login.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
+</head>
+
+<body class="loginpage">
+    <header class="flex">
+        <div class="maatschappijinfo flex">
+            <img src="assets/img/ETAirways.png" alt="ETplaatje" height="80" width="80">
+            <div class="maatschappijtekst">
+                <h1>ET Airways</h1>
+            </div>
+            <div class="taalinloggen flex position">
+                <div class="taal flex">
+                    <img src="assets/img/nlvlag.png" alt="nl" width="27" height="20">
+                    <div class="nltekst">
+                        <h2>Netherlands - nl</h2>
+                    </div>
+                </div>
+                <div class="inloggen flex">
+                    <div class="inlogplaatje">
+                        <img src="assets/img/inlog.png" alt="inlog" width="32" height="32">
+                    </div>
+                    <div class="nltekst">
+                        <h2>Inloggen</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="hoofdteksten flex">
+            <div class="pijlteruglogin flex position">
+                <h1>←</h1>
+            </div>
+            <div class="logintekst flex position">
+                <h5>Log in met uw gegevens</h5>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="loginachtergrond"></div>
+        <div class="loginborder flex position"></div>
+        <div class="logintekstborder flex">
+
+        </div>
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+
+</html>
