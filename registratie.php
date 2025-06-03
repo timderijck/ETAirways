93f2f4720e06ba8c8e879c64f65d656eae6842c4
timderijck <timderijck2@gmail.com>
2025-06-03 10:47:54 +0200

    registratie.php toegevoegd, alle login paginas af in html/css

---
+++ registratie.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
+</head>
+
+<body class="loginpage">
+    <header class="flex">
+        <div class="maatschappijinfo flex">
+            <img src="assets/img/ETAirways.png" alt="ETplaatje" height="80" width="80">
+            <div class="maatschappijtekst">
+                <h1>ET Airways</h1>
+            </div>
+            <div class="taalinloggen flex position">
+                <div class="taal flex">
+                    <img src="assets/img/nlvlag.png" alt="nl" width="27" height="20">
+                    <div class="nltekst">
+                        <h2>Netherlands - nl</h2>
+                    </div>
+                </div>
+                <div class="inloggen flex">
+                    <div class="inlogplaatje">
+                        <img src="assets/img/inlog.png" alt="inlog" width="32" height="32">
+                    </div>
+                    <div class="nltekst">
+                        <h2>Inloggen</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="hoofdteksten flex">
+            <div class="pijlteruglogin flex position">
+                <h1>←</h1>
+            </div>
+            <div class="logintekst flex position">
+                <h5>Maak een account aan</h5>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="loginachtergrond flex"></div>
+        <div class="loginborder flex position"></div>
+        <div class="logintekstborder flex">
+            <div class="volgordeborder flex">
+                <h5>Registreren</h5>
+                <div class="verticalpadding"></div>
+                <input type="text" id="gebruikersnaam" name="gebruikersnaam" placeholder="Gebruikersnaam" class="noborder">
+                <div class="verticalpadding"></div>
+                <input type="text" id="password" name="password" placeholder="Password" class="noborder">
+                <div class="verticalpadding"></div>
+                <input type="text" id="confirmpassword" name="confirmpassword" placeholder="Confirm password" class="noborder">
+            </div>
+        </div>
+        <div class="loginenadminlogin flex">
+            <div class="loginknop2">
+                <button class="loginknop">Aanmaken</button class="loginknop">
+            </div>
+        </div>
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+
+</html>
