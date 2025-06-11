9729b26d49eb576a0376dd43db605b53512dbe63
emile25 <1212164@student.roc-nijmegen.nl>
2025-06-11 09:49:38 +0200

    begin admin pagina en voorbereiding voor php

---
+++ admin.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
+</head>
+
+<body class="body-admin">
+    
+    <header class="flex">
+        <div class="maatschappijinfo flex">
+            <img src="assets/img/ETAirways.png" alt="ETplaatje" height="80" width="80">
+            <div class="maatschappijtekst">
+                <h1>ET Airways</h1>
+            </div>
+            <div class="taalinloggen flex position">
+                <div class="taal flex">
+                    <img src="assets/img/nlvlag.png" alt="nl" width="27" height="20">
+                    <div class="nltekst">
+                        <h2>Netherlands - nl</h2>
+                    </div>
+                </div>
+                <div class="inloggen flex">
+                    <div class="inlogplaatje position">
+                        <img src="assets/img/inlog.png" alt="inlog" width="32" height="32">
+                    </div>
+                    <div class="nltekst">
+                        <h2>Inloggen</h2>
+                        <div class="dropdown">
+                            <button class="dropbtn"><h2>Inloggen</h2>
+                                <i class="fa fa-caret-down"></i>
+                            </button>
+                            <div class="dropdown-content">
+                                <a href="registratie.php"><h2>Account aanmaken</h2></a>
+                                <a href="login.php"><h2>Inloggen</h2></a>
+                                <a href="adminlogin.php"><h2>Admin</h2></a>
+                            </div>
+                        </div>
+                    </div>
+
+            </div>
+        </div>
+      
+
+      
+        
+
+        
+
+   
+    </main>
+
+
+</body>
+
+</html>
